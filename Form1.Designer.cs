14bf411f9229797ba99dbb3ee3e53236230bdc58
Mateo <mateo.santarsiero@gmail.com>
2024-05-12 17:50:09 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            minus = new System.Windows.Forms.Button();
+            multi = new System.Windows.Forms.Button();
+            div = new System.Windows.Forms.Button();
+            seven = new System.Windows.Forms.Button();
+            four = new System.Windows.Forms.Button();
+            one = new System.Windows.Forms.Button();
+            eight = new System.Windows.Forms.Button();
+            five = new System.Windows.Forms.Button();
+            two = new System.Windows.Forms.Button();
+            zero = new System.Windows.Forms.Button();
+            nine = new System.Windows.Forms.Button();
+            six = new System.Windows.Forms.Button();
+            three = new System.Windows.Forms.Button();
+            equals = new System.Windows.Forms.Button();
+            plus = new System.Windows.Forms.Button();
+            equationLabel = new System.Windows.Forms.Label();
+            AC = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // minus
+            // 
+            minus.BackColor = System.Drawing.Color.White;
+            minus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            minus.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            minus.Location = new System.Drawing.Point(310, 314);
+            minus.Name = "minus";
+            minus.Size = new System.Drawing.Size(93, 51);
+            minus.TabIndex = 1;
+            minus.Text = "-";
+            minus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            minus.UseVisualStyleBackColor = false;
+            minus.Click += minus_Click;
+            // 
+            // multi
+            // 
+            multi.BackColor = System.Drawing.Color.White;
+            multi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            multi.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            multi.Location = new System.Drawing.Point(310, 260);
+            multi.Name = "multi";
+            multi.Size = new System.Drawing.Size(93, 51);
+            multi.TabIndex = 2;
+            multi.Text = "X";
+            multi.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            multi.UseVisualStyleBackColor = false;
+            multi.Click += multi_Click;
+            // 
+            // div
+            // 
+            div.BackColor = System.Drawing.Color.White;
+            div.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            div.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            div.Location = new System.Drawing.Point(310, 203);
+            div.Name = "div";
+            div.Size = new System.Drawing.Size(93, 51);
+            div.TabIndex = 3;
+            div.Text = "/";
+            div.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            div.UseVisualStyleBackColor = false;
+            div.Click += div_Click;
+            // 
+            // seven
+            // 
+            seven.BackColor = System.Drawing.Color.White;
+            seven.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            seven.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            seven.Location = new System.Drawing.Point(13, 203);
+            seven.Name = "seven";
+            seven.Size = new System.Drawing.Size(93, 51);
+            seven.TabIndex = 4;
+            seven.Text = "7";
+            seven.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            seven.UseVisualStyleBackColor = false;
+            seven.Click += seven_Click;
+            // 
+            // four
+            // 
+            four.BackColor = System.Drawing.Color.White;
+            four.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            four.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            four.Location = new System.Drawing.Point(13, 260);
+            four.Name = "four";
+            four.Size = new System.Drawing.Size(93, 51);
+            four.TabIndex = 5;
+            four.Text = "4";
+            four.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            four.UseVisualStyleBackColor = false;
+            four.Click += four_Click;
+            // 
+            // one
+            // 
+            one.BackColor = System.Drawing.Color.White;
+            one.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            one.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            one.Location = new System.Drawing.Point(13, 314);
+            one.Name = "one";
+            one.Size = new System.Drawing.Size(93, 51);
+            one.TabIndex = 6;
+            one.Text = "1";
+            one.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            one.UseVisualStyleBackColor = false;
+            one.Click += one_Click;
+            // 
+            // eight
+            // 
+            eight.BackColor = System.Drawing.Color.White;
+            eight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            eight.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            eight.Location = new System.Drawing.Point(112, 203);
+            eight.Name = "eight";
+            eight.Size = new System.Drawing.Size(93, 51);
+            eight.TabIndex = 7;
+            eight.Text = "8";
+            eight.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            eight.UseVisualStyleBackColor = false;
+            eight.Click += button7_Click;
+            // 
+            // five
+            // 
+            five.BackColor = System.Drawing.Color.White;
+            five.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            five.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            five.Location = new System.Drawing.Point(112, 260);
+            five.Name = "five";
+            five.Size = new System.Drawing.Size(93, 51);
+            five.TabIndex = 8;
+            five.Text = "5";
+            five.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            five.UseVisualStyleBackColor = false;
+            five.Click += five_Click;
+            // 
+            // two
+            // 
+            two.BackColor = System.Drawing.Color.White;
+            two.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            two.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            two.Location = new System.Drawing.Point(112, 314);
+            two.Name = "two";
+            two.Size = new System.Drawing.Size(93, 51);
+            two.TabIndex = 9;
+            two.Text = "2";
+            two.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            two.UseVisualStyleBackColor = false;
+            two.Click += two_Click;
+            // 
+            // zero
+            // 
+            zero.BackColor = System.Drawing.Color.White;
+            zero.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            zero.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            zero.Location = new System.Drawing.Point(13, 376);
+            zero.Name = "zero";
+            zero.Size = new System.Drawing.Size(93, 51);
+            zero.TabIndex = 10;
+            zero.Text = "0";
+            zero.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            zero.UseVisualStyleBackColor = false;
+            zero.Click += zero_Click;
+            // 
+            // nine
+            // 
+            nine.BackColor = System.Drawing.Color.White;
+            nine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            nine.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            nine.Location = new System.Drawing.Point(211, 203);
+            nine.Name = "nine";
+            nine.Size = new System.Drawing.Size(93, 51);
+            nine.TabIndex = 12;
+            nine.Text = "9";
+            nine.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            nine.UseVisualStyleBackColor = false;
+            nine.Click += nine_Click;
+            // 
+            // six
+            // 
+            six.BackColor = System.Drawing.Color.White;
+            six.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            six.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            six.Location = new System.Drawing.Point(211, 260);
+            six.Name = "six";
+            six.Size = new System.Drawing.Size(93, 51);
+            six.TabIndex = 13;
+            six.Text = "6";
+            six.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            six.UseVisualStyleBackColor = false;
+            six.Click += six_Click;
+            // 
+            // three
+            // 
+            three.BackColor = System.Drawing.Color.White;
+            three.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            three.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            three.Location = new System.Drawing.Point(211, 314);
+            three.Name = "three";
+            three.Size = new System.Drawing.Size(93, 51);
+            three.TabIndex = 14;
+            three.Text = "3";
+            three.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            three.UseVisualStyleBackColor = false;
+            three.Click += button14_Click;
+            // 
+            // equals
+            // 
+            equals.BackColor = System.Drawing.Color.White;
+            equals.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            equals.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            equals.Location = new System.Drawing.Point(211, 376);
+            equals.Name = "equals";
+            equals.Size = new System.Drawing.Size(93, 51);
+            equals.TabIndex = 15;
+            equals.Text = "=";
+            equals.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            equals.UseVisualStyleBackColor = false;
+            equals.Click += equals_Click;
+            // 
+            // plus
+            // 
+            plus.BackColor = System.Drawing.Color.White;
+            plus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            plus.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            plus.Location = new System.Drawing.Point(310, 376);
+            plus.Name = "plus";
+            plus.Size = new System.Drawing.Size(93, 51);
+            plus.TabIndex = 16;
+            plus.Text = "+";
+            plus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            plus.UseVisualStyleBackColor = false;
+            plus.Click += plus_Click;
+            // 
+            // equationLabel
+            // 
+            equationLabel.AutoSize = true;
+            equationLabel.BackColor = System.Drawing.Color.White;
+            equationLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            equationLabel.Font = new System.Drawing.Font("Consolas", 25.8000011F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            equationLabel.Location = new System.Drawing.Point(3, 9);
+            equationLabel.MinimumSize = new System.Drawing.Size(400, 50);
+            equationLabel.Name = "equationLabel";
+            equationLabel.Size = new System.Drawing.Size(400, 51);
+            equationLabel.TabIndex = 0;
+            equationLabel.Click += label1_Click;
+            // 
+            // AC
+            // 
+            AC.BackColor = System.Drawing.Color.White;
+            AC.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            AC.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            AC.Location = new System.Drawing.Point(310, 146);
+            AC.Name = "AC";
+            AC.Size = new System.Drawing.Size(93, 51);
+            AC.TabIndex = 17;
+            AC.Text = "AC";
+            AC.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            AC.UseVisualStyleBackColor = false;
+            AC.Click += AC_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = System.Drawing.Color.White;
+            button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            button1.Location = new System.Drawing.Point(112, 376);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(93, 51);
+            button1.TabIndex = 18;
+            button1.Text = ".";
+            button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.DarkGray;
+            BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            ClientSize = new System.Drawing.Size(410, 435);
+            Controls.Add(button1);
+            Controls.Add(AC);
+            Controls.Add(plus);
+            Controls.Add(equals);
+            Controls.Add(three);
+            Controls.Add(six);
+            Controls.Add(nine);
+            Controls.Add(zero);
+            Controls.Add(two);
+            Controls.Add(five);
+            Controls.Add(eight);
+            Controls.Add(one);
+            Controls.Add(four);
+            Controls.Add(seven);
+            Controls.Add(div);
+            Controls.Add(multi);
+            Controls.Add(minus);
+            Controls.Add(equationLabel);
+            Name = "Form1";
+            Text = "Calculadora ";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        internal System.Windows.Forms.Button minus;
+        internal System.Windows.Forms.Button multi;
+        internal System.Windows.Forms.Button div;
+        internal System.Windows.Forms.Button seven;
+        internal System.Windows.Forms.Button four;
+        internal System.Windows.Forms.Button one;
+        internal System.Windows.Forms.Button eight;
+        internal System.Windows.Forms.Button five;
+        internal System.Windows.Forms.Button two;
+        internal System.Windows.Forms.Button zero;
+        internal System.Windows.Forms.Button point;
+        internal System.Windows.Forms.Button nine;
+        internal System.Windows.Forms.Button six;
+        internal System.Windows.Forms.Button three;
+        internal System.Windows.Forms.Button equals;
+        internal System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Label equationLabel;
+        internal System.Windows.Forms.Button AC;
+        internal System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "3";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "8";
+
+        }
+
+        private void equals_Click(object sender, EventArgs e)
+        {
+            string equation = equationLabel.Text;
+            if (equation.Equals(""))
+            {
+                MessageBox.Show("Ingrese una función");
+            }
+            else
+            {
+                equationLabel.Text = Evaluate(equation).ToString();
+            }
+        }
+        private static dynamic Evaluate(string expression)
+        {
+            DataTable dt = new DataTable();
+            dynamic answer = dt.Compute(expression, "");
+            return (dynamic)answer;
+        }
+
+        private void AC_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text = "";
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "0";
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "9";
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += " / ";
+        }
+
+        private void multi_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += " * ";
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += " - ";
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += " + ";
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "6";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "7";
+
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "4";
+
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "5";
+
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "1";
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += "2";
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            equationLabel.Text += ".";
+        }
+    }
+}
